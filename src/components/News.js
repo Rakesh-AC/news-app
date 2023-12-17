d6fe280ef1477c3c718abbb684c41eaad8a2acbc
Rakesh-AC <rakesh.ac@parachutetech.ai>
2023-12-17 20:07:07 +0530

    Fetched data from the newsApi and displayed it in the cards

--- src/components/News.js
+++ src/components/News.js
@@ -5,11 +5,14 @@ export default class News extends Component {
   render() {
     return (
       <div>
-        <h2>This is news component</h2>
-        <NewsItem />
-        <NewsItem />
-        <NewsItem />
-        <NewsItem />
+        <div className="container">
+          <h2>NewsMonkey - Top Headlines </h2>
+          <div className="row">
+            <div className="col-md-4"><NewsItem title="my title" description="my description of the news" /></div>
+            <div className="col-md-4"><NewsItem title="my title" description="my description of the news" /></div>
+            <div className="col-md-4"><NewsItem title="my title" description="my description of the news" /></div>
+          </div>
+        </div>
       </div>    
     )
   }
--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -2,9 +2,17 @@ import React, { Component } from 'react'
 
 export default class NewsItem extends Component {
   render() {
+    let {title , description} = this.props;
     return (
       <div>
-        <h4 className='text-muted'>This is news Item</h4>
+        <div className="card" style={{width: "18rem"}}>
+          <img src="https://www.tcpalm.com/gcdn/authoring/authoring-images/2023/09/25/PBRE/70958148007-img-1041.jpeg?crop=4021,2259,x0,y168&width=3200&height=1798&format=pjpg&auto=webp" className="card-img-top" alt="..." />
+          <div className="card-body">
+            <h5 className="card-title">Card title</h5>
+            <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <a href="/newsDetails" className="btn btn-primary">Go somewhere</a>
+          </div>
+        </div>
       </div>
     )
   }
