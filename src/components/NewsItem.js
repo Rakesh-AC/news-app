d382efb9c6beef36d638ec76852c8cc494859f8b
Rakesh-AC <rakesh.ac@parachutetech.ai>
2023-12-17 13:48:08 +0530

    Created basic news components structure

---
+++ src/components/NewsItem.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class NewsItem extends Component {
+  render() {
+    return (
+      <div>
+        <h4 className='text-muted'>This is news Item</h4>
+      </div>
+    )
+  }
+}
--- src/index.js
+++ src/index.js
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import NavBar from './components/NavBar';
+import News from './components/News';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <NavBar />
+    <News />
   </React.StrictMode>
 );
 
