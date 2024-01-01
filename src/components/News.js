dff5b9d82954a31b353bb5a4e68f4d92a47eae6e
Rakesh-AC <rakesh.ac@parachutetech.ai>
2024-01-01 18:06:19 +0530

    Added categories in the navigation section

--- src/components/News.js
+++ src/components/News.js
@@ -1,7 +1,18 @@
 import React, { Component } from 'react';
 import NewsItem from './NewsItem';
+import PropTypes from 'prop-types'; 
 
 export default class News extends Component {
+
+  static defaultProps = {
+    country : "in",
+    category : "general"
+  }
+
+  static propTypes = {
+    country: PropTypes.string,
+    category: PropTypes.string,
+  }
 
   constructor() {
     super();
@@ -20,7 +31,7 @@ export default class News extends Component {
     }else{
       console.log("Next click");
       this.setState({loading:true})
-      let url = `https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=c4535d9de63941d88609c6ef86fb843b&page=${this.state.page +1}&pageSize=9`
+      let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=c4535d9de63941d88609c6ef86fb843b&page=${this.state.page +1}&pageSize=9`
       let data = await fetch(url);
       let parsedData = await data.json();
       console.log(parsedData);
@@ -31,7 +42,7 @@ export default class News extends Component {
   handlePrevClick = async () =>{
       this.setState({loading:true})
       console.log("Previous click");
-      let url = `https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=c4535d9de63941d88609c6ef86fb843b&page=${this.state.page -1}&pageSize=9`
+      let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=c4535d9de63941d88609c6ef86fb843b&page=${this.state.page -1}&pageSize=9`
       let data = await fetch(url);
       let parsedData = await data.json();
       console.log(parsedData);
@@ -43,7 +54,7 @@ export default class News extends Component {
 
   async componentDidMount(){
     this.setState({loading:true})
-    let url = `https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=c4535d9de63941d88609c6ef86fb843b&page=${this.state.page}&pageSize=9`
+    let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=c4535d9de63941d88609c6ef86fb843b&page=${this.state.page}&pageSize=9`
     let data = await fetch(url);
     let parsedData = await data.json();
     console.log(parsedData);
@@ -63,7 +74,7 @@ export default class News extends Component {
           <h2 className='text-center'>NewsMonkey - Top Headlines (t-{this.state.articles.length})</h2>
 
           <div className="row">
-            {loading ? (<div className="container mt-3 d-flex justify-content-center"><div class="spinner" ></div></div> ) : (
+            {loading ? (<div className="container mt-3 d-flex justify-content-center"><div class="spinner"></div></div> ) : (
               this.state.articles.map(ele => (
                 (ele.title !== null || ele.title !== "")  && (ele.content !== null || ele.content !== "") && ele.urlToImage !== null && ele.url !==null &&
                 <div className="col-md-4 mt-3" key={ele.url}>
